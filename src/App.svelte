844bdd662cde4bd08c396267aebd75844a59daa5
AH134 <github.com.uo5n8@aleeas.com>
2025-04-24 12:22:26 -0700

    fix: remove reset button toggling all characters

--- src/App.svelte
+++ src/App.svelte
@@ -27,7 +27,9 @@
 
     const resetFilters = () => {
         randomizedCharacters = [];
-        toggleSelection();
+        if (!isAllSelected) {
+            toggleSelection();
+        }
     };
 
     const generateRandomizedCharacters = () => {
